5fe15e6de30a1d1d0d5dc33c3f75dfd4d49c116c
yasuko <y.otsubo04@gmail.com>
2025-04-03 19:37:30 +0900

    RHK（ログイン）

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,30 +2,39 @@
 
 import { supabase } from "@/utils/supabase";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
 
-export default function Page () {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+type FormData = {
+  email: string;
+  password: string;
+}
+
+export default function FormBasic() {
   const router = useRouter();
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+     const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+     });
 
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
+     const onsubmit: SubmitHandler<FormData> = async (data) => {
+      const { error } = await supabase.auth.signInWithPassword({
+        email: data.email,
+        password: data.password,
+      });
 
-    if(error) {
-      alert('ログインに失敗しました');
-    } else {
-      router.push('/admin/posts');
-    }
-  }
-  return (
+      if(error) {
+        alert('ログインに失敗しました');
+      } else {
+        router.push('/admin/posts');
+      }
+     };
+
+     const onerror: SubmitErrorHandler<FormData> = (errors) => {
+      console.log(errors);
+     }
+
+      return (
     <div className="flex justify-center pt-[240px]">
-      <form onSubmit={handleSubmit} className="space-y-4 w-full max-w-[400px]">
+      <form onSubmit={handleSubmit(onsubmit, onerror)} noValidate className="space-y-4 w-full max-w-[400px]">
         <div>
           <label
             htmlFor="email"
@@ -35,13 +44,21 @@ export default function Page () {
           </label>
           <input
             type="email"
+            {...register('email', {
+              required: 'emailは必須入力です',
+              maxLength: {
+                value: 100,
+                message: 'emailは100字以内で入力してください'
+              }
+            })}
             name="email"
             id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="name@company.com"
             required
-            onChange={(e) => setEmail(e.target.value)}
+            //onChange={(e) => setEmail(e.target.value)}
           />
+          <div>{errors.email?.message}</div>
         </div>
         <div>
           <label
@@ -52,13 +69,16 @@ export default function Page () {
           </label>
           <input
             type="password"
+            {...register('password', {
+              required: 'パスワードは必須です',
+            })}
             name="password"
             id="password"
             placeholder="••••••••"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             required
-            onChange={(e) => setPassword(e.target.value)}
           />
+          <div>{errors.password?.message}</div>
         </div>
 
         <div>
@@ -72,4 +92,4 @@ export default function Page () {
       </form>
     </div>
   )
-}
+} 
